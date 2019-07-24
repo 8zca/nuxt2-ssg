fee2ad855e2c34ff3df1be2bafc78d5d01a0699c
Moriwaki Kazuya <kazuya.m@sora.flights>
2019-07-24 20:48:10 +0900

    summaryをループ処理化

--- src/components/pages/dashboard/index.vue
+++ src/components/pages/dashboard/index.vue
@@ -4,31 +4,28 @@
       h1 Dashboard
     v-flex(xs12)
       v-layout(row wrap)
-        v-flex(d-flex xs6 md4)
+        v-flex(d-flex xs6 md4 v-for="row in summary" :key="row.title")
           v-card(color="red lighten-1" flat dark)
             v-layout
               v-flex(xs4)
                 v-card-text
-                  v-icon(size="70") thumb_up
+                  v-icon(size="70")
               v-flex(xs8)
-                v-card-title.display-2(primary) 25%
-                v-card-text DAILY ACTIVE USERS
-        v-flex(d-flex xs6 md4)
-          v-card(color="red lighten-1" falt dark)
-            v-layout
-              v-flex(xs4)
-                v-card-text
-                  v-icon(size="70") thumb_down
-              v-flex(xs8)
-                v-card-title.display-2(primary) 10%
-                v-card-text MONTHLY ACTIVE USERS
-        v-flex(d-flex xs6 md4)
-          v-card(color="green lighten-1" flat dark)
-            v-layout
-              v-flex(xs4)
-                v-card-text
-                  v-icon(size="70") account_circle
-              v-flex(xs8)
-                v-card-title.display-2(primary) 70%
-                v-card-text ACTIVE HOTELS
+                v-card-title.display-2(primary) {{ row.value }}
+                v-card-text {{ row.value }}
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      summary: [
+        { title: 'Today Transaction Count', value: 1234 },
+        { title: 'Weekly Transaction Count', value: 9999 },
+        { title: 'Today Transaction Total Amount', value: 12340000 },
+        { title: 'Weekly Transaction Total Amount', value: 12340000 }
+      ]
+    }
+  }
+}
+</script>
