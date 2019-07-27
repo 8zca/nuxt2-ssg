cd0fa08e605b2bff5f08e4c3b37d98d6606ed2a6
Moriwaki Kazuya <kazuya.m@sora.flights>
2019-07-27 23:14:25 +0900

    css調整

--- src/components/pages/dashboard/index.vue
+++ src/components/pages/dashboard/index.vue
@@ -4,15 +4,11 @@
       h1 Dashboard
     v-flex(xs12)
       v-layout(row wrap)
-        v-flex(d-flex xs6 md4 v-for="row in summary" :key="row.title")
-          v-card(color="red lighten-1" flat dark)
-            v-layout
-              v-flex(xs4)
-                v-card-text
-                  v-icon(size="70")
-              v-flex(xs8)
-                v-card-title.display-2(primary) {{ row.value }}
-                v-card-text {{ row.value }}
+        v-flex(d-flex xs6 md3 v-for="row in summary" :key="row.title")
+          v-card(color="grey lighten-4")
+            v-card-text
+              div.sm-title {{ row.title }}
+              div.sm-value {{ row.value }}
 </template>
 
 <script>
@@ -23,9 +19,20 @@ export default {
         { title: 'Today Transaction Count', value: 1234 },
         { title: 'Weekly Transaction Count', value: 9999 },
         { title: 'Today Transaction Total Amount', value: 12340000 },
-        { title: 'Weekly Transaction Total Amount', value: 12340000 }
+        { title: 'Weekly Transaction Total Amount', value: 99990000 }
       ]
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.sm-title {
+  font-size: 11px;
+  margin-bottom: 8px;
+}
+.sm-value {
+  font-size: 18px;
+  text-align: right;
+}
+</style>
