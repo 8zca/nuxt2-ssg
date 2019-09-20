0a70f4e32ff7404e0402b84cda04a3773f5b2eea
Moriwaki Kazuya <kazuya.m@sora.flights>
2019-09-21 00:27:03 +0900

    Navigation correction

--- src/components/templates/Sidebar.vue
+++ src/components/templates/Sidebar.vue
@@ -3,21 +3,23 @@
     v-toolbar(dark flat color="primary")
       v-toolbar-title(@click="") #[v-icon.mr-2 data_usage] Dashboard
     v-list
-      template(v-for="route in routes")
-        template(v-if="isShow(route)")
-          template(v-if="hasOneChild(route)")
-            v-list-item
-              v-list-item-icon
-                v-icon {{ route.children[0].icon }}
-              v-list-item-title {{ route.children[0].title }}
-          template(v-else)
-            v-list-group(:prepend-icon="route.icon")
-              template(v-slot:activator)
-                v-list-item-title {{ route.title }}
-              v-list-item(v-for="child in route.children" :key="route.name")
-                v-list-item-title {{ child.title }}
+      v-list-item-group
+        template(v-for="route in routes")
+          template(v-if="isShow(route)")
+            template(v-if="hasOneChild(route)")
+              v-list-item
                 v-list-item-icon
-                  v-icon {{ child.icon }}
+                  v-icon {{ route.children[0].icon }}
+                v-list-item-content
+                  v-list-item-title {{ route.children[0].title }}
+            template(v-else)
+              v-list-group(:prepend-icon="route.icon" value="true")
+                template(v-slot:activator)
+                  v-list-item-title {{ route.title }}
+                v-list-item(v-for="child in route.children" :key="route.name")
+                  v-list-item-title {{ child.title }}
+                  v-list-item-icon
+                    v-icon {{ child.icon }}
 </template>
 
 <script>
